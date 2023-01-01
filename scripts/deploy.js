7fa5090fb8f150b8df4c19f49867c38771923aec
MaheshMannava <maheshmannava16@gmail.com>
2023-01-01 20:51:04 +0530

    init

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy("0xc4dCB5126a3AfEd129BC3668Ea19285A9f56D15D");
+
+  await flashLoan.deployed();
+
+  console.log("Flashloan contract deployed to " + flashLoan.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
